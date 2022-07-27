5bd3db086f3b68a0ce06ef616c34d8e3fd956bab
TechnicallyCoded <mathiastech.jar@gmail.com>
2022-07-27 16:05:05 +0200

    Fix URLs not being clickable in chat

--- src/main/java/ml/tcoded/nochatreports/listener/ChatListener.java
+++ src/main/java/ml/tcoded/nochatreports/listener/ChatListener.java
@@ -2,11 +2,7 @@ package ml.tcoded.nochatreports.listener;
 
 import ml.tcoded.nochatreports.event.AsyncNonReportableChatEvent;
 import ml.tcoded.nochatreports.event.AsyncPostNonReportableChatEvent;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -39,14 +35,13 @@ public class ChatListener implements Listener {
         // Parse result
         UUID uuid = noReportChatEvent.getPlayer().getUniqueId();
         String formattedMsg = String.format(noReportChatEvent.getFormat(), noReportChatEvent.getPlayer().getDisplayName(), noReportChatEvent.getMessage());
-        BaseComponent msgComponent = new TextComponent(formattedMsg);
 
         // Log chat to console since cancelling the event causes the chat to not be logged
         Bukkit.getConsoleSender().sendMessage(formattedMsg);
 
         // Send the message to recipients
         for (Player player : noReportChatEvent.getRecipients()) {
-            player.spigot().sendMessage(ChatMessageType.CHAT, uuid, msgComponent);
+            player.sendMessage(uuid, formattedMsg);
         }
 
         // Fire event with final message that was sent to the players
