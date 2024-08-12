af7023911db8a44cbceff56fdd0c82db314fd13f
TechnicallyCoded <technicallycoded@gmail.com>
2024-08-12 18:42:56 +0200

    v2.1.0 - 1.21.1 support

---
+++ nms/v1_21_1/src/main/java/com/tcoded/nochatreports/nms/v1_21_1/NMS_v1_21_1.java
@@ -0,0 +1,41 @@
+package com.tcoded.nochatreports.nms.v1_21_1;
+
+import com.tcoded.nochatreports.nms.NmsProvider;
+import com.tcoded.nochatreports.nms.wrapper.PlayerChatPacket;
+import com.tcoded.nochatreports.nms.wrapper.SystemChatPacket;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+@SuppressWarnings("unused")
+public class NMS_v1_21_1 extends NmsProvider<ServerPlayer> {
+
+    private final boolean isPaper;
+
+    public NMS_v1_21_1(boolean isPaper) {
+        this.isPaper = isPaper;
+    }
+
+    @Override
+    public PlayerChatPacket wrapChatPacket(ByteBuf byteBuf) {
+        return new PlayerChatPacketImpl(byteBuf);
+    }
+
+    @Override
+    public void sendSystemPacket(Player player, SystemChatPacket systemPacket) {
+        ServerPlayer nmsPlayer = getNmsPlayer(player);
+        Packet<?> nmsPacket = (Packet<?>) systemPacket.toNmsPacket();
+
+        nmsPlayer.connection.sendPacket(nmsPacket);
+    }
+
+    @Override
+    public ServerPlayer getNmsPlayer(Player player) {
+        CraftPlayer craftPlayer = (CraftPlayer) player;
+        if (isPaper) return (ServerPlayer) craftPlayer.getHandleRaw();
+        else return craftPlayer.getHandle();
+    }
+
+}
